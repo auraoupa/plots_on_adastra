2fe07f32de91d2cc22c5112b2c9985673b8475c5
auraoupa <auraoupa@gmail.com>
2023-07-21 16:50:47 +0200

    up

--- one_plot_eORCA05.py
+++ one_plot_eORCA05.py
@@ -15,19 +15,17 @@ import matplotlib.ticker as mticker
 import cmocean
 import dask
 
-
-
 filein = '/lus/work/NAT/gda2307/aalbert/eORCA05.L121/eORCA05.L121-AGDA004-S/1h/2012/eORCA05.L121-AGDA004_y2012m01d01.1h_gridT.nc'
 filemask = '/lus/work/NAT/gda2307/aalbert/eORCA05.L121/eORCA05.L121-I/eORCA05.L121_mesh_mask_4.2.nc'
 var = 'votemper'
 lev = 0
-ttime = 0 
-proj = ccrs.Orthographic(central_longitude=-30,central_latitude=35)
+ttime = 10 
+proj = ccrs.Orthographic(central_latitude=30.0, central_longitude=-30.0)
 reg = 'global'
 cmap = cmap=cmocean.cm.thermal
 vmin = -2
 vmax = 30
-title = 'Surface temperature at lev = '+str(lev)+' adn time = '+str(ttime)
+title = 'Surface temperature at lev = '+str(lev)+' and time = '+str(ttime)
 plotname ='plots/map_temp_eORCA05.L121-AGDA004_lev'+str(lev)+'_time'+str(ttime)+'.png'
 
 fig = plt.figure(figsize=(15,15))
@@ -54,7 +52,7 @@ def one_plot_surf(fig, sub, filein, filemask, var, lev, ttime, proj, reg, cmap, 
 
     datam=np.ma.array(data,mask=1-tmask)
 
-    ax = fig.add_subplot(sub,projection=ccrs.PlateCarree(central_longitude=-30))
+    ax = fig.add_subplot(sub,projection=proj)
 
     if reg == 'global':
         ax.set_global()
@@ -63,24 +61,22 @@ def one_plot_surf(fig, sub, filein, filemask, var, lev, ttime, proj, reg, cmap, 
     elif reg == 'natl':
         ax.set_extent([-100, 50, 50, 70])
 
-    pcolor=ax.pcolormesh(lon,lat,ma.masked_invalid(datam),transform=ccrs.PlateCarree(),cmap=cmap,vmin=vmin,vmax=vmax)
-    ax.add_feature(cfeature.LAND,facecolor='grey')
-    ax.coastlines()
-    gl=ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-                  linewidth=2, color='gray', alpha=0.5, linestyle='--')
+    pcolor=ax.pcolormesh(lon,lat,datam,transform=ccrs.PlateCarree(),shading='flat',cmap=cmap,vmin=vmin,vmax=vmax)
+#    ax.add_feature(cfeature.LAND,facecolor='grey')
+#    ax.coastlines()
+#    gl=ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+#                  linewidth=0.5, color='gray', alpha=0.5)
+#    gl.top_labels   = False
+#    gl.right_labels = False
+#    gl.bottom_labels   = False
+#    gl.left_labels = False
+#    gl.xlocator = plt.FixedLocator(range(-180, 181, 20))
+#    gl.ylocator = plt.FixedLocator(range(-90, 91, 30))
 
     fig.subplots_adjust(right=0.8)
-    gl.xlocator = mticker.FixedLocator([-110,-90,-70,-50,-30,-10,10,30,50,70,90,110])
-    gl.ylocator = mticker.FixedLocator([-10,0,10,20,30,40,50,60,70,80])
-    gl.xformatter = LONGITUDE_FORMATTER
-    gl.yformatter = LATITUDE_FORMATTER
-    gl.xlabel_style = {'size': 15, 'color': 'gray'}
-    gl.ylabel_style = {'size': 15, 'color': 'gray'}
-    gl.top_labels = False
-    gl.left_labels = False
 
-    cbar = plt.colorbar(pcolor,orientation='horizontal',shrink=0.75,label=unit)
+    cbar = plt.colorbar(pcolor,orientation='vertical',fraction=0.026, pad=0.1,label=unit)
     ax.set_title(title,size=17,y=1.08)
 
 
-plt.savefig('plot_sst_eORCA05.png')
+plt.savefig(plotname)
